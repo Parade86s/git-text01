83cb1f6343ee075e1570dbd416a3bc5eba1ce3ca
Wesley <Wesley@gmail.com>
2021-06-19 21:11:46 +0800

    aaa

---
+++ src/main/java/com/wesley/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.wesley.git;
+
+/**
+ * @author 曹澳
+ * @version V1.0
+ * @Package com.wesley.git
+ * @date 2021/6/19 0019 21:04
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
